414f27fc9c905055eda791ee7657572cbe2d09dd
Selvaganapathi Kannan <selvaganapathi@mallow-tech.com>
2020-08-08 14:29:55 +0530

    upadte and await concept learned and applied

--- src/app.js
+++ src/app.js
@@ -18,3 +18,44 @@ app.post('/users',(req, res) => {
   })
   res.send(req.body)
 })
+
+
+app.get('/users', (req,res) => {
+  let user= new Login()
+  Login.find({}).then((res) => {
+     console.log(res)
+     res.send(res)
+
+  }).catch(()=> {
+    res.status(500).status()
+    console.log('Error on getting user')
+  })
+})
+
+app.get('/users/:id', async (req, res) => {
+  console.log(req.params)
+  try {
+    let getData =  await Login.findById(req.params.id)
+    res.send(getData)
+  } catch(e){
+     console.log(e)
+  }
+  // let user = new Login(req.body);
+  // Login.findById(req.params.id).then( (user) => {
+  //   console.log('saved successfully')
+  //   res.send(user)
+  // }).catch((error) => {
+  //   console.log(error)
+  // })
+})
+
+
+app.patch('/users', async (req, res) => {
+ try {
+   console.log(req.body)
+  let data = await Login.findByIdAndUpdate(req.body.id, {name: req.body.name}, {new: true})
+  res.send(data)
+ } catch(e) {
+    console.log(e)
+ }
+})
